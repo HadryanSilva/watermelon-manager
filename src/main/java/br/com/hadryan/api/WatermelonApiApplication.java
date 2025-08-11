1a9b4573979edb812c33fd7245055c9d5e73fd90
Hadryan Silva <hadryan2001@gmail.com>
2025-08-10 21:04:20 -0300

    Initial Commit

---
+++ src/main/java/br/com/hadryan/api/WatermelonApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.hadryan.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatermelonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatermelonApiApplication.class, args);
+	}
+
+}
